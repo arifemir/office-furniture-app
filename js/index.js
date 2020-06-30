cf9c8bbb8372203dd4d64402195ddb61a3825072
aearslanturk <arifarslanturk@gmail.com>
2020-07-01 01:58:34 +0300

    formated and added favicon

--- js/index.js
+++ js/index.js
@@ -1,39 +1,38 @@
-const navbar = document.getElementById('navbar');
+const navbar = document.getElementById("navbar");
 
-const lightbox = document.createElement('div');
-lightbox.id = 'lightbox';
+const lightbox = document.createElement("div");
+lightbox.id = "lightbox";
 document.body.appendChild(lightbox);
 
-const images = document.querySelectorAll('.lb');
-images.forEach(img => {
-  img.addEventListener('click',e => {
-    lightbox.classList.add('active');
-    let image = document.createElement('img');
+const images = document.querySelectorAll(".lb");
+images.forEach((img) => {
+  img.addEventListener("click", (e) => {
+    lightbox.classList.add("active");
+    let image = document.createElement("img");
     image.src = img.src;
     lightbox.appendChild(image);
-  })
-})
+  });
+});
 
-lightbox.addEventListener('click',function(e) {
-  if(e.target !== e.currentTarget) return
-  lightbox.classList.remove('active');
-  lightbox.innerHTML = '';
-})
+lightbox.addEventListener("click", function (e) {
+  if (e.target !== e.currentTarget) return;
+  lightbox.classList.remove("active");
+  lightbox.innerHTML = "";
+});
 
 let scrolled = false;
-window.onscroll = function() {
-  if(window.pageYOffset > 100) {
-    navbar.classList.remove('top');
-    if(!scrolled) {
-      navbar.style.transform = 'translateY(-70px)';
+window.onscroll = function () {
+  if (window.pageYOffset > 100) {
+    navbar.classList.remove("top");
+    if (!scrolled) {
+      navbar.style.transform = "translateY(-70px)";
     }
-    setTimeout(function() {
-      navbar.style.transform = 'translateY(0px)';
+    setTimeout(function () {
+      navbar.style.transform = "translateY(0px)";
       scrolled = true;
     }, 200);
   } else {
-    navbar.classList.add('top');
+    navbar.classList.add("top");
     scrolled = false;
   }
-}
-
+};
